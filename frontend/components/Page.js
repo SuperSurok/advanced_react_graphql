d338e33b6a0ccb02e5fb652826093b56fc7e184b
Sergey Lebedev <oddyone@mail.ru>
2018-12-01 14:58:53 +0300

    Introduction to Styled Components.

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import Header from "../components/Header";
 import Meta from "../components/Meta";
+
+const MyBtutton = styled.button`
+  background: red;
+  font-size: 100px;
+  border-radius: 5px;
+  box-shadow: 10px 10px 5px 5px greenyellow;
+`;
 
 class Page extends Component {
   render() {
@@ -8,6 +16,7 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyBtutton>Click Me!</MyBtutton>
         {this.props.children}
       </div>
     );
