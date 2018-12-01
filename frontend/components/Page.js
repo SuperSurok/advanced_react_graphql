b24e1cc1e8ee48feb097d7c5b9e1cdd2e8c5e95c
Sergey Lebedev <oddyone@mail.ru>
2018-12-01 21:31:56 +0300

    Global Styling and Typography with Styled Components.

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import React, {Component} from "react";
+import styled, {ThemeProvider, injectGlobal} from "styled-components";
 import Header from "../components/Header";
 import Meta from "../components/Meta";
 
 const theme = {
-  red: "#ff0000",
-  black: "#393939",
-  lightgrey: "#e1e1e1",
-  offWhite: "#ededed",
-  maxWidth: "1000px",
-  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+    red: "#ff0000",
+    black: "#393939",
+    lightgrey: "#e1e1e1",
+    offWhite: "#ededed",
+    maxWidth: "1000px",
+    bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
 };
 
 const StyledPage = styled.div`
@@ -18,23 +18,50 @@ const StyledPage = styled.div`
 `;
 
 const Inner = styled.div`
-  max-width: ${props => props.theme.maxWidth };
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
 `;
 
-class Page extends Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <StyledPage>
-          <Meta />
-          <Header />
-            <Inner>{this.props.children}</Inner>
-        </StyledPage>
-      </ThemeProvider>
-    );
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url("../static/radnikanext-medium-webfont.woff2") format('woff2');
+    font-weight: normal;
+    font-style: normal;  
   }
+  html {
+      -webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;
+      font-size: 10px;
+  }
+  *, *:before, *:after {
+    -webkit-box-sizing: inherit;-moz-box-sizing: inherit;box-sizing: inherit;
+  }
+  body {
+      padding: 0;
+      margin: 0;
+      font-size: 1.5em; 
+      line-height: 2;
+      font-family: 'radnika_next';
+  }
+  a{
+  text-decoration: none;
+  color: ${theme.black}
+  }
+`;
+
+class Page extends Component {
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <StyledPage>
+                    <Meta/>
+                    <Header/>
+                    <Inner>{this.props.children}</Inner>
+                </StyledPage>
+            </ThemeProvider>
+        );
+    }
 }
 
 export default Page;
