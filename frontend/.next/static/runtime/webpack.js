0bf8d48839709986833abc84764d738b4c5d06f8
Sergey Lebedev <oddyone@mail.ru>
2018-12-17 23:46:38 +0300

    Create items with mutations. Part 3.

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ccc4ea5b385de8af341c";
+/******/ 	var hotCurrentHash = "2b9c32aa4cf951d255b8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
